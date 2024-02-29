5d2e12d5162fc949c4daebac70d546430381fd4a
habini86 <vinicius1234585@gmail.com>
2024-02-28 21:15:34 -0300

    fix: retirada de um console.log

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,6 @@ export const routes = [
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const { search } = req.query 
-      console.log(search)
 
       const tasks = database.select('tasks', search ? {
         title: search,
